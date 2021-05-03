36299bd0fe123d6e3a5318a9f9acfd16564c26c1
Jimmy Song <jaejoon@gmail.com>
2021-05-03 10:32:43 -0500

    add support for SLIP39

--- src/embit/util/pyhashlib.py
+++ src/embit/util/pyhashlib.py
@@ -1,11 +1,33 @@
 import hmac
 import hashlib
 
+# xor two arrays
+def binxor(a, b):
+    return bytes([x ^ y for (x, y) in zip(a, b)])
+
+
 # https://en.wikipedia.org/wiki/PBKDF2
 def pbkdf2_hmac_sha512(password, salt, iterations: int, bytes_to_read: int):
-    # xor two arrays
-    def binxor(a, b):
-        return bytes([x ^ y for (x, y) in zip(a, b)])
+    # convert to bytes
+    if isinstance(password, str):
+        password = password.encode("utf-8")
+    if isinstance(salt, str):
+        salt = salt.encode("utf-8")
+    # result
+    r = b""
+    for i in range(1, bytes_to_read // 64 + 1 + int(bool(bytes_to_read % 64))):
+        current = hmac.new(
+            password, salt + i.to_bytes(4, "big"), digestmod=hashlib.sha512
+        ).digest()
+        result = current
+        for j in range(2, 1 + iterations):
+            current = hmac.new(password, current, digestmod=hashlib.sha512).digest()
+            result = binxor(result, current)
+        r += result
+    return r[:bytes_to_read]
+
+# https://en.wikipedia.org/wiki/PBKDF2
+def pbkdf2_hmac_sha256(password, salt, iterations: int, bytes_to_read: int):
 
     # convert to bytes
     if isinstance(password, str):
@@ -15,13 +37,13 @@ def pbkdf2_hmac_sha512(password, salt, iterations: int, bytes_to_read: int):
     # result
     r = b""
     for i in range(1, bytes_to_read // 64 + 1 + int(bool(bytes_to_read % 64))):
-        U = hmac.new(
-            password, salt + i.to_bytes(4, "big"), digestmod=hashlib.sha512
+        current = hmac.new(
+            password, salt + i.to_bytes(4, "big"), digestmod=hashlib.sha256
         ).digest()
-        result = U
+        result = current
         for j in range(2, 1 + iterations):
-            U = hmac.new(password, U, digestmod=hashlib.sha512).digest()
-            result = binxor(result, U)
+            current = hmac.new(password, current, digestmod=hashlib.sha256).digest()
+            result = binxor(result, current)
         r += result
     return r[:bytes_to_read]
 
